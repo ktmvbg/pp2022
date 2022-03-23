efb5e00674ac4769fe4b313bb67ab743820459ee
ktmvbg <101252555+ktmvbg@users.noreply.github.com>
2022-03-23 15:52:31 +0700

    Add files via upload

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,64 +1,115 @@
-def getStudentCount ():
-    return int (input ("Enter number of students: "))
-    
-def getCourseCount ():
-    return int (input ("Enter number of courses: "))
-    
-def getCourseDetails ():
-    cid = input ("Enter course id: ")
-    cname = input ("Enter course name: ")
-    return cid, cname
-    
-def getStudentDetails ():
-    sid = input ("Enter student id: ")
-    sname = input ("Enter student name: ")
-    sdob = input ("Enter student's date of birth: ")
-    return sid, sname, sdob 
-    
-def getMarks (sid, cid):
-    prompt = f"Enter marks for student {sid} for course {cid}: ".format (sid, cid)
-    input (prompt)
-    
-nStudents = getStudentCount ()
-studentLst = []
-for i in range (nStudents):
-    sid, nm, dob = getStudentDetails ()
-    studentLst.append ((sid, nm, dob))
+def get_number_of_students():
+    number_of_students = int(input("Enter number of students: "))
+    return number_of_students
 
-nCourses = getCourseCount ()
-courseLst = []
-for i in range (nCourses):
-    cid, cname = getCourseDetails ()
-    courseLst.append ((cid, cname))
-    
-d = {}    
-n = int (input ("Enter how many student-course marks do you want to enter? "))
-for i in range (n):
-    while True:
-        sid = input ("Enter student id: ")
-        cid = input ("Enter course id: ")
-        if sid not in [student [0] for student in studentLst]:
-            print ("Bad student id")
-            continue 
-        if cid not in [course [0] for course in courseLst]:
-            print ("Bad course id")
-            continue 
-        break
-    marks = int (input ("Enter marks: "))
-    if cid in d:
-        d [cid].append ((sid, marks))
-    else:
-        d [cid] = [(sid, marks)]
-        
-print ("\nListing all students now..")
-for s in studentLst:
-    print (f"Student id: {s[0]} Name: {s[1]} Date of birth: {s[2]}")
+def get_student_information():
+    student_id = input("Enter student id: ")
+    student_name = input("Enter student name: ")
+    student_dob = input("Enter student DoB: ")
+    return (student_id, student_name, student_dob)
 
-print ("\nListing all courses now..")
-for c in courseLst:
-    print (f"Course id: {c[0]} Name: {c[1]}")
-    
-cid = input ("\nWhich course do you want to see all student marks for? ")
-if cid in d:
-    for tups in d [cid]:
-        print (f"Student {tups[0]} got {tups [1]} marks")
+def get_students(number_of_students):
+    students = []
+    for i in range(0, number_of_students):
+        while(1):
+            student = get_student_information()
+            dup = len([x for x in students if x[0] == student[0]]) > 0
+            if(dup):
+                print("Invalid student id")
+            else:
+                students.append(student)
+                print("Added: {}".format(student))
+                break
+    return students
+
+def list_students(students):
+    print("Number of students: {0}".format(len(students)))
+    for student in students:
+        print(student)
+
+def get_number_of_courses():
+    number_of_courses = int(input("Enter number of courses: "))
+    return number_of_courses
+
+def get_course_information():
+    course_id = input("Enter course id: ")
+    course_name = input("Enter course name: ")
+    return (course_id, course_name)
+
+def get_courses(number_of_courses):
+    courses = []
+    for i in range(0, number_of_courses):
+        while(1):
+            course = get_course_information()
+            dup = len([x for x in courses if x[0] == course[0]]) > 0
+            if(dup):
+                print("Invalid course id")
+            else:
+                courses.append(course)
+                print("Added: {}".format(course))
+                break
+    return courses
+
+def list_courses(courses):
+    print("Number of courses: {0}".format(len(courses)))
+    for course in courses:
+        print(course)
+
+def get_number_of_marks():
+    number_of_marks = int(input("Enter number of marks: "))
+    return number_of_marks
+
+def get_mark():
+    student_id = input("Enter student id: ")
+    course_id = input("Enter course id: ")
+    mark = float(input("Enter mark: "))
+    return (student_id, course_id, mark)
+
+def get_marks(number_of_marks, students, courses):
+    marks = []
+    for i in range(number_of_marks):
+        while(1):
+            mark = get_mark()
+            s = len([x for x in students if x[0] == mark[0]])
+            if(s == 0):
+                print("Invalid student id")
+                continue
+            c = len([x for x in courses if x[0] == mark[1]])
+            if(c == 0):
+                print("Invalid course id")
+                continue
+            m = len([x for x in marks if x[0] == mark[0] and x[1] == mark[1]])
+            if(m > 0):
+                print("Duplicate")
+                continue
+            marks.append(mark)
+            print("Added: {}".format(mark))
+            break
+    return marks
+
+def list_mark(marks):
+    print("Number of marks: {0}".format(len(marks)))
+    for mark in marks:
+        print(mark)
+
+def list_mark_by_id(course_id, marks):
+    m = [x for x in marks if x[1] == course_id]
+    print("Number of marks course_id = {0}: {1}".format(course_id, len(m)))
+    for mark in m:
+        print(mark)
+
+def main():
+    number_of_students = get_number_of_students()
+    students = get_students(number_of_students)
+    list_students(students)
+    number_of_courses = get_number_of_courses()
+    courses = get_courses(number_of_courses)
+    list_courses(courses)
+    number_of_marks = get_number_of_marks()
+    marks = get_marks(number_of_marks, students, courses)
+    list_mark(marks)
+    list_mark_by_id("1", marks)
+
+
+if __name__ == "__main__":
+    main()
